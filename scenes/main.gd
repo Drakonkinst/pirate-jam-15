cca9332774145f688f77de90f30a5667a0358232
Drakonkinst <drakonkinstmc@gmail.com>
2024-07-14 14:45:11 -0700

    Add basic scenes

---
+++ scenes/main.gd
@@ -0,0 +1,12 @@
+extends Node
+
+@export var game_scene: PackedScene
+
+var game: Game
+
+func _ready() -> void:
+    start_new_game()
+
+func start_new_game() -> void:
+    game = game_scene.instantiate()
+    add_child(game)
