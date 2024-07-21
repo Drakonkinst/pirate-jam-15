dc8ce141688f02e9f680b6c061f97aa0c24e8c78
leauxgan1 <logan.langdon@yahoo.com>
2024-07-20 20:16:42 -0700

    Added main menu with initial menu sounds

--- scenes/enemy/shadow_enemy.gd
+++ scenes/enemy/shadow_enemy.gd
@@ -5,8 +5,8 @@ extends Node2D
 @export var row = 0 
 
 enum EnemyState {
-    MOVING,
-    ATTACKING,
+	MOVING,
+	ATTACKING,
 }
 
 var state = EnemyState.MOVING
@@ -17,35 +17,35 @@ var obstacle: Obstacle = null
 @onready var sprite = %Sprite
 
 func _ready():
-    pass
+	pass
 
 func _process(delta):
-    var adjacent_tile = get_left_tile()
-    if adjacent_tile and adjacent_tile.obstacle != null:
-        obstacle = adjacent_tile.obstacle
-        state = EnemyState.ATTACKING
-    else:
-        state = EnemyState.MOVING
+	var adjacent_tile = get_left_tile()
+	if adjacent_tile and adjacent_tile.obstacle != null:
+		obstacle = adjacent_tile.obstacle
+		state = EnemyState.ATTACKING
+	else:
+		state = EnemyState.MOVING
 
-    match state:
-        EnemyState.MOVING:
-            walk(delta)
-        EnemyState.ATTACKING:
-            attack(obstacle)
+	match state:
+		EnemyState.MOVING:
+			walk(delta)
+		EnemyState.ATTACKING:
+			attack(obstacle)
 
 func get_left_tile() -> GridTile:
-    var size_x = sprite.texture.get_width() * sprite.scale.x
-    var detection_offset = Vector2(-size_x, 0)
-    var shadow_left_position: Vector2 = global_position + detection_offset
+	var size_x = sprite.texture.get_width() * sprite.scale.x
+	var detection_offset = Vector2(-size_x, 0)
+	var shadow_left_position: Vector2 = global_position + detection_offset
 
-    var tile: GridTile = GlobalVariables.curr_game.grid.screenspace_to_tile(shadow_left_position)
-    return tile
+	var tile: GridTile = GlobalVariables.curr_game.grid.screenspace_to_tile(shadow_left_position)
+	return tile
 
 func attack(target_obstacle: Obstacle):
-    # TODO: Implement small timer
-    if target_obstacle != null:
-        target_obstacle.damage(1)
+	# TODO: Implement small timer
+	if target_obstacle != null:
+		target_obstacle.damage(1)
 
 func walk(delta):
-    global_position += Vector2(-move_speed * delta,0)
+	global_position += Vector2(-move_speed * delta,0)
 
