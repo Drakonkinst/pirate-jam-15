0d47c1a8c2fb03b5626c40bb417bcfa5e5067a4b
leauxgan1 <logan.langdon@yahoo.com>
2024-07-27 04:07:16 -0700

    Separated enemies into two kinds which inherit from a base Enemy class

---
+++ scenes/enemy/enemy.gd
@@ -0,0 +1,48 @@
+extends CharacterBody2D
+
+class_name Enemy
+
+@export var move_speed = 1500.0
+@export var row = 0 
+@export var enemy_data: EnemyData
+
+var moving: bool = true
+var target_tile: GridTile = null
+
+@export var state_machine: StateMachine
+@onready var sprite = %Sprite
+@onready var health = %Health
+
+func _ready():
+	health.set_max_health(enemy_data.health)
+	health.set_health(enemy_data.health)
+
+func _physics_process(_delta):
+	move_and_slide()
+
+func get_left_tile() -> GridTile:
+	var size_x = sprite.texture.get_width() * sprite.scale.x
+	var detection_offset = Vector2(-size_x / 4, 0)
+	var shadow_left_position: Vector2 = global_position + detection_offset
+
+	var tile: GridTile = GlobalVariables.get_grid().screenspace_to_tile(shadow_left_position)
+	return tile
+
+func get_current_tile() -> GridTile:
+	var tile: GridTile = GlobalVariables.get_grid().screenspace_to_tile(global_position)
+	return tile
+
+func attack(tile: GridTile) -> bool:
+	if tile == null or tile.obstacle == null:
+		return false
+	tile.obstacle.damage(enemy_data.attack_damage)
+	return true
+
+func damage(val):
+	health.damage(val)
+	print(health.health)
+
+
+func _on_health_death():
+	GlobalVariables.get_pickup_manager().spawn_pickup_drop(Pickup.PickupType.GOLD,global_position,1)
+	queue_free()
--- scenes/projectile/bolt.gd
+++ scenes/projectile/bolt.gd
@@ -8,14 +8,14 @@ class_name MagicBolt
 @export var bolt_damage: float = 75.0
 
 func init(from: Vector2, to: Vector2):
-    global_position = from
-    direction = (to - from).normalized()
-    look_at(to)
+	global_position = from
+	direction = (to - from).normalized()
+	look_at(to)
 
 func _process(delta: float) -> void:
-    position += direction * speed * delta
+	position += direction * speed * delta
 
 func _on_hitbox_body_entered(body: Node2D) -> void:
-    if body is ShadowEnemy:
-        body.damage(bolt_damage)
-        queue_free()
+	if body is Enemy:
+		body.damage(bolt_damage)
+		queue_free()
