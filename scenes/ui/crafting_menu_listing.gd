d88f5df6dfe6db9840b1244bdb464978ee2b7d72
leauxgan1 <logan.langdon@yahoo.com>
2024-07-29 03:05:14 -0700

    Fixed crafting menu to actually deduct resources

--- scenes/ui/crafting_menu_listing.gd
+++ scenes/ui/crafting_menu_listing.gd
@@ -4,16 +4,17 @@ class_name CraftingMenuListing
 
 # Signals for playing effects when crafting begins, progresses, or ends
 signal crafting_start
+signal crafting_failed
 signal crafting_timer_down
 signal crafting_end
 
 @onready var crafting_timer: Timer = %CraftingTimer
 @onready var craft_button: Button = %Button
 
-
 @export var crafting_data: CraftingData
 @onready var crafting_time: float = crafting_data.crafting_time
 
+@onready var player: Player = get_tree().get_nodes_in_group(GlobalVariables.PLAYER_GROUP)[0]
 
 @export var quartz_img: CompressedTexture2D
 @export var stone_img: CompressedTexture2D
@@ -22,76 +23,106 @@ signal crafting_end
 
 @export var ally_img: CompressedTexture2D
 
+@export var potion_menu: Control 
 
+
+static var is_crafting := false
 
 func _ready():
-	setup()
+    setup()
 
 func setup():
-	%ItemLabel.text = CraftingData.ItemType.keys()[crafting_data.item]
-	var item_texture: CompressedTexture2D
-	match crafting_data.item:
-		CraftingData.ItemType.POTION_QUARTZ:
-			item_texture = quartz_img
-		CraftingData.ItemType.POTION_STONE:
-			item_texture = stone_img
-		CraftingData.ItemType.POTION_OIL:
-			item_texture = oil_img
-		CraftingData.ItemType.POTION_WOOD:
-			item_texture = wood_img
-		CraftingData.ItemType.ALLY_SPRITE:
-			item_texture = ally_img
-	%ItemTexture.texture = item_texture
-			
-	
+    %ItemLabel.text = CraftingData.ItemType.keys()[crafting_data.item]
+    var item_texture: CompressedTexture2D
+    match crafting_data.item:
+        CraftingData.ItemType.POTION_QUARTZ:
+            item_texture = quartz_img
+        CraftingData.ItemType.POTION_STONE:
+            item_texture = stone_img
+        CraftingData.ItemType.POTION_OIL:
+            item_texture = oil_img
+        CraftingData.ItemType.POTION_WOOD:
+            item_texture = wood_img
+        CraftingData.ItemType.ALLY_SPRITE:
+            item_texture = ally_img
+    %ItemTexture.texture = item_texture
 
 func _on_craft_button_pressed():
-	start_crafting()
+    start_crafting()
 
 func start_crafting():
-	crafting_start.emit()
-	crafting_time = crafting_data.crafting_time
-	craft_button.text = str(crafting_time)
-	
-	crafting_timer.wait_time = 1
-	crafting_timer.start()
+    if is_crafting:
+        return
+    if try_expend_resources() == false:
+        crafting_failed.emit()
+        return
+    is_crafting = true
+    crafting_start.emit()
+    crafting_time = crafting_data.crafting_time
+    craft_button.text = str(crafting_time)
+    
+    crafting_timer.wait_time = 1
+    crafting_timer.start()
+
+func try_expend_resources() -> bool:
+    var crafting_recipe = crafting_data.recipe_list
+    var player_resources = player.player_resources
+
+    # Check if player has enough resources 
+    for i in range(0,crafting_recipe.size()):
+        var crafting_component = crafting_recipe[i]
+        var cost = crafting_component.count
+        var held_resource = player_resources.get_count(crafting_component.resource_type)
+        if cost > held_resource:
+            print("Not enough resources")
+            return false
+
+    # Reduce resources if so
+    for i in range(0,crafting_recipe.size()):
+        var crafting_component = crafting_recipe[i]
+        var cost = crafting_component.count
+        var resource_type = crafting_component.resource_type
+        player_resources.set_count(resource_type,player_resources.get_count(resource_type) - cost)
+        
+    return true
 
 func end_crafting():
-	crafting_end.emit()
-	craft_button.text = "Craft"
-	give_item()
+    is_crafting = false
+    crafting_end.emit()
+    craft_button.text = "Craft"
+    give_item()
 
 func give_item():
-	var tool_inv = GlobalVariables.get_toolbar().tool_inventory
-	# Dont mind the below code it does the thing
-	var proj_type: ThrownProjectile.Type
-	var summon_type: EnemySpawner.EnemyType
-	match crafting_data.item:
-		CraftingData.ItemType.POTION_WOOD:
-			proj_type = ThrownProjectile.Type.POTION_WOOD
-		CraftingData.ItemType.POTION_STONE:
-			proj_type = ThrownProjectile.Type.POTION_STONE
-		CraftingData.ItemType.POTION_QUARTZ:
-			proj_type = ThrownProjectile.Type.POTION_QUARTZ
-		CraftingData.ItemType.POTION_OIL:
-			proj_type = ThrownProjectile.Type.POTION_OIL
-		CraftingData.ItemType.ALLY_SPRITE:
-			summon_type = EnemySpawner.EnemyType.RockSprite
-	if proj_type:
-		tool_inv.set_potion_count(proj_type, tool_inv.get_potion_count(proj_type) + 1)
-	elif summon_type:
-		tool_inv.set_summon_count(summon_type, tool_inv.get_summon_count(summon_type) + 1)
+    var tool_inv = GlobalVariables.get_toolbar().tool_inventory
+    # Dont mind the below code it does the thing
+    var proj_type: ThrownProjectile.Type
+    var summon_type: EnemySpawner.EnemyType
+    match crafting_data.item:
+        CraftingData.ItemType.POTION_WOOD:
+            proj_type = ThrownProjectile.Type.POTION_WOOD
+        CraftingData.ItemType.POTION_STONE:
+            proj_type = ThrownProjectile.Type.POTION_STONE
+        CraftingData.ItemType.POTION_QUARTZ:
+            proj_type = ThrownProjectile.Type.POTION_QUARTZ
+        CraftingData.ItemType.POTION_OIL:
+            proj_type = ThrownProjectile.Type.POTION_OIL
+        CraftingData.ItemType.ALLY_SPRITE:
+            summon_type = EnemySpawner.EnemyType.RockSprite
+    if proj_type:
+        tool_inv.set_potion_count(proj_type, tool_inv.get_potion_count(proj_type) + 1)
+    elif summon_type:
+        tool_inv.set_summon_count(summon_type, tool_inv.get_summon_count(summon_type) + 1)
 
 
 func _on_crafting_timer_timeout():
-	if crafting_time > 1:
-		#Decrement timer
-		crafting_time -= 1
-		craft_button.text = str(crafting_time)
-		crafting_timer_down.emit()
-		
-		crafting_timer.wait_time = 1
-		crafting_timer.start()
-	else:
-		# Drop new item at cauldron for pickup
-		end_crafting()
+    if crafting_time > 1:
+        #Decrement timer
+        crafting_time -= 1
+        craft_button.text = str(crafting_time)
+        crafting_timer_down.emit()
+        
+        crafting_timer.wait_time = 1
+        crafting_timer.start()
+    else:
+        # Drop new item at cauldron for pickup
+        end_crafting()
