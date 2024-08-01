f5968ca36336c589dbf8af54921eee62c18a9fa0
Drakonkinst <drakonkinstmc@gmail.com>
2024-08-01 01:37:28 -0700

    Fix scoring and add PirateJam logo

--- scenes/menus/game_over_menu.gd
+++ scenes/menus/game_over_menu.gd
@@ -8,18 +8,17 @@ signal restart
 @export var game_result_label: Label
 @export var score_text: Label
 @export var enemies_killed_text: Label
-# TODO: Rounds survived or time survived
 
 func enable() -> void:
 	var sprites_killed = GlobalVariables.curr_game.sprites_killed
 	var golems_killed = GlobalVariables.curr_game.golems_killed
 	var game_result = GlobalVariables.curr_game.game_result
+	var score = GlobalVariables.curr_game.score
 	game_result_label.text = game_result
 
 	var enemies_killed = str(golems_killed + sprites_killed)
 	enemies_killed_text.text = "Enemies Defeated: " + str(enemies_killed)
 
-	var score = 10 * golems_killed + sprites_killed
 	score_text.text = "Score: " + str(score)
 	show()
 
