e03d1793b679cfff4fa727c943a1dff68034dc57
Drakonkinst <drakonkinstmc@gmail.com>
2024-08-09 00:28:06 -0700

    Framework for data-driven tools

---
+++ data/tool/tool_data.gd
@@ -0,0 +1,12 @@
+extends Resource
+
+class_name ToolData
+
+# TODO: Move tools to this system
+
+@export var id: ToolBar.Tool
+@export var name: String
+@export var description: String
+@export var equip_audio: AudioStream
+@export var use_audio: AudioStream
+@export var cooldown: float
